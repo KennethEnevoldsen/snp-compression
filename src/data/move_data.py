bb05883ba28a376789c86fc8862131aea5bcf95e
KennethEnevoldsen <kennethcenevoldsen@gmail.com>
2021-05-27 13:52:16 +0200

    added mv data src

--- src/data/move_data.py
+++ src/data/move_data.py
@@ -1,50 +1,17 @@
 """
+This script move the data to the right folder 
 """
+
 
 import os
 import shutil
 from pathlib import Path
 
-original = os.path.join(Path.home(), "NLPPred")
-files = [os.path.join(original, f) for f in os.listdir(original) if f.lower().endswith(".bed")]
-target = os.path.join(Path.home(), "github", "snp-compression", "data", "raw")
+os.chdir("/home/kce/github/snp-compression")
+origin = os.path.join(Path.home(), "NLPPred")
+files = [f for f in os.listdir(origin) if f.split(".")[-1] in {"bed", "fam", "bim"}]
 
 for f in files:
-    shutil.move(f, target)
-
-print("[INFO] Files in raw:")
-print(os.listdir(target))
-
-files = [os.path.join(target, f) for f in os.listdir(target) if f.lower().endswith(".bed")]
-
-
-with open(files[0], "rb") as f:
-    text = f.read()
-
-text.decode(errors="ignore")[:100]
-
-import ast
-import csv
-
-
-def _parse_bytes(field):
-    """ Convert string represented in Python byte-string literal b'' syntax into
-        a decoded character string - otherwise return it unchanged.
-    """
-    result = field
-    try:
-        result = ast.literal_eval(field)
-    finally:
-        return result.decode() if isinstance(result, bytes) else field
-
-
-def my_csv_reader(filename, /, **kwargs):
-    with open(filename, 'rt', newline='') as file:
-        for row in csv.reader(file, **kwargs):
-            yield [_parse_bytes(field) for field in row]
-
-
-reader = my_csv_reader(files[0], delimiter='\t')
-for row in reader:
-    print(row)
-
+    src = os.path.join(origin, f)
+    tgt = os.path.join("data", "raw", f)
+    shutil.copy(src, tgt)
