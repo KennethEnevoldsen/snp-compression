0fbc89b6ef3bf2118c0d635aedf618e55b9dc0fd
KennethEnevoldsen <kennethcenevoldsen@gmail.com>
2022-02-11 17:31:23 +0100

    Exclude NAs from tests

--- src/models/models/pl_wrappers.py
+++ src/models/models/pl_wrappers.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
@@ -15,8 +16,11 @@ class PlOnehotWrapper(pl.LightningModule):
         self.lr = learning_rate
         self.accuracy = torchmetrics.Accuracy()
         self.f1 = torchmetrics.F1(num_classes=num_classes, mdmc_average="global")
+        self.pearson = torchmetrics.PearsonCorrCoef()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.nan_to_num(x, nan=3)
+        # x.shape should be (batch, channels=1, genotype/snp=4, sequence length)
         return self.model(x)
 
     def configure_optimizers(self):
@@ -26,40 +30,54 @@ class PlOnehotWrapper(pl.LightningModule):
     def training_step(self, train_batch, batch_idx):
         s = time.time()
         x = train_batch
-        x = torch.nan_to_num(x, nan=3)
+        # x.shape should be (batch, sequence length)
 
-        # y shape should be (batch, sequence length)
-        # x should be the one hot encoded version of y
-
-        # in shape should be (batch, channels=1, genotype/snp=4, sequence length)
         x_hat = self.forward(x)
-        x = x.type(torch.LongTensor).to(self.device)
-        # out shape should be (batch, genotype/snp=4, sequence length)
-        loss = self.loss(x_hat, x)
-        self.log("train_loss", loss)
+        # x.shape should be (batch, genotype/snp=4, sequence length)
 
+        # remove NAs
+        x, x_hat = self.__remove_NAs(x, x_hat)
+
+        # calculate metrics
+        x.type(torch.LongTensor).to(self.device)
+        loss = self.loss(x_hat, x)
         probs = x_hat.softmax(dim=1)
         preds = probs.argmax(dim=1)
+
+        self.log("train_loss", loss)
         self.log("train_acc", self.accuracy(probs, x))
         self.log("train_f1", self.f1(preds, x))
+        self.log("val_cor", self.pearson(preds, x))
         self.log("train_step/sec", time.time() - s)
 
         return loss
 
+    def __remove_NAs(
+        self, x: torch.Tensor, x_hat: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        valid = ~x.isnan()
+        x = x[valid]
+        valid_one_hot = valid.unsqueeze(1).expand((-1, x[1], -1))
+        x_hat = x_hat[valid_one_hot]
+
     def validation_step(self, val_batch, batch_idx):
+        s = time.time()
         x = val_batch
-        x = torch.nan_to_num(x, nan=3)
 
-        # y shape should be (batch, sequence length)
-
-        # in shape should be (batch, channels=1, genotype/snp=4, sequence length)
         x_hat = self.forward(x)
-        x = x.type(torch.LongTensor).to(self.device)
-        loss = self.loss(x_hat, x)
+        # x.shape should be (batch, genotype/snp=4, sequence length)
 
-        self.log("val_loss", loss)
+        # remove NAs
+        x, x_hat = self.__remove_NAs(x, x_hat)
+
+        # calculate metrics
+        x.type(torch.LongTensor).to(self.device)
+        loss = self.loss(x_hat, x)
         probs = x_hat.softmax(dim=1)
         preds = probs.argmax(dim=1)
 
+        self.log("val_loss", loss)
         self.log("val_acc", self.accuracy(probs, x))
         self.log("val_f1", self.f1(preds, x))
+        self.log("val_cor", self.pearson(preds, x))
+        self.log("val_step/sec", time.time() - s)
